89b91245a38fefb0e0b184a65d763bf8b185b34c
David Su <david.d.su@gmail.com>
2023-06-08 09:29:37 -0700

    Make UpdateEngineJob just a function (which we can then submit as a task to the thread pool)

--- SoundEnginePlugin/EngineUpdater.cpp
+++ SoundEnginePlugin/EngineUpdater.cpp
@@ -2,42 +2,54 @@
 
 #include "RaveWwiseFX.h"
 
-UpdateEngineJob::UpdateEngineJob(RaveWwiseFX& processor, const std::string modelFile)
-	: ThreadPoolJob("UpdateEngineJob"), mProcessor(processor),
-	mModelFile(modelFile) {}
+namespace RAVEWwise
+{
+	void UpdateEngineJob(RaveWwiseFX* processor, const std::string& modelFile)
+	{
+		// TODO: waitForFadeOut()?
 
-UpdateEngineJob::~UpdateEngineJob() {}
-
-bool UpdateEngineJob::waitForFadeOut(size_t waitTimeMs) {
-	// TODO
-	//for (size_t i = 0; i < waitTimeMs && mProcessor.getIsMuted(); ++i) {
-	//  juce::Thread::sleep(1);
-	//}
-	//return (mProcessor.getIsMuted());
-
-	return false;
+		processor->_rave->load_model(modelFile);
+		processor->updateBufferSizes();
+		processor->unmute();
+	}
 }
 
-auto UpdateEngineJob::runJob() -> JobStatus {
-	if (shouldExit()) {
-		return JobStatus::jobNeedsRunningAgain;
-	}
-
-	// TODO
-	//mProcessor.mute();
-
-	while (!waitForFadeOut(1)) {
-		if (shouldExit()) {
-			return JobStatus::jobNeedsRunningAgain;
-		}
-	}
-
-	// TODO
-	//mProcessor._rave->load_model(mModelFile);
-	//mProcessor.updateBufferSizes();
-	//mProcessor.unmute();
-
-	DBG("Job finished");
-
-	return JobStatus::jobHasFinished;
-}
+//UpdateEngineJob::UpdateEngineJob(RaveWwiseFX& processor, const std::string modelFile)
+//	: ThreadPoolJob("UpdateEngineJob"), mProcessor(processor),
+//	mModelFile(modelFile) {}
+//
+//UpdateEngineJob::~UpdateEngineJob() {}
+//
+//bool UpdateEngineJob::waitForFadeOut(size_t waitTimeMs) {
+//	// TODO
+//	//for (size_t i = 0; i < waitTimeMs && mProcessor.getIsMuted(); ++i) {
+//	//  juce::Thread::sleep(1);
+//	//}
+//	//return (mProcessor.getIsMuted());
+//
+//	return false;
+//}
+//
+//auto UpdateEngineJob::runJob() -> JobStatus {
+//	if (shouldExit()) {
+//		return JobStatus::jobNeedsRunningAgain;
+//	}
+//
+//	// TODO
+//	//mProcessor.mute();
+//
+//	while (!waitForFadeOut(1)) {
+//		if (shouldExit()) {
+//			return JobStatus::jobNeedsRunningAgain;
+//		}
+//	}
+//
+//	// TODO
+//	//mProcessor._rave->load_model(mModelFile);
+//	//mProcessor.updateBufferSizes();
+//	//mProcessor.unmute();
+//
+//	DBG("Job finished");
+//
+//	return JobStatus::jobHasFinished;
+//}
--- SoundEnginePlugin/EngineUpdater.h
+++ SoundEnginePlugin/EngineUpdater.h
@@ -1,21 +1,42 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include <string>
 
 class RaveWwiseFX; // forward declaration
 
-// TODO: Replace juce::ThreadPoolJob with custom implementation
-class UpdateEngineJob : public juce::ThreadPoolJob {
-public:
-	explicit UpdateEngineJob(RaveWwiseFX& processor, const std::string modelPath);
-	virtual ~UpdateEngineJob();
-	virtual auto runJob()->JobStatus;
-	bool waitForFadeOut(size_t waitTimeMs);
+namespace RAVEWwise
+{
+	void UpdateEngineJob(RaveWwiseFX* processor, const std::string& modelFile);
+}
 
-private:
-	RaveWwiseFX& mProcessor;
-	const std::string mModelFile;
-	// Prevent uncontrolled usage
-	UpdateEngineJob(const UpdateEngineJob&);
-	UpdateEngineJob& operator=(const UpdateEngineJob&);
-};
+
+// TODO: Replace juce::ThreadPoolJob with custom implementation
+//class UpdateEngineJob : public juce::ThreadPoolJob {
+//public:
+//	explicit UpdateEngineJob(RaveWwiseFX& processor, const std::string modelPath);
+//	virtual ~UpdateEngineJob();
+//	virtual auto runJob()->JobStatus;
+//	bool waitForFadeOut(size_t waitTimeMs);
+//
+//private:
+//	RaveWwiseFX& mProcessor;
+//	const std::string mModelFile;
+//	// Prevent uncontrolled usage
+//	UpdateEngineJob(const UpdateEngineJob&);
+//	UpdateEngineJob& operator=(const UpdateEngineJob&);
+//};
+//// TODO: Replace juce::ThreadPoolJob with custom implementation
+//class UpdateEngineJob : public juce::ThreadPoolJob {
+//public:
+//	explicit UpdateEngineJob(RaveWwiseFX& processor, const std::string modelPath);
+//	virtual ~UpdateEngineJob();
+//	virtual auto runJob()->JobStatus;
+//	bool waitForFadeOut(size_t waitTimeMs);
+//
+//private:
+//	RaveWwiseFX& mProcessor;
+//	const std::string mModelFile;
+//	// Prevent uncontrolled usage
+//	UpdateEngineJob(const UpdateEngineJob&);
+//	UpdateEngineJob& operator=(const UpdateEngineJob&);
+//};
