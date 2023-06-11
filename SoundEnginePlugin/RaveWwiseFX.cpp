9d4c8b8565397e7a94eb1e480dd90efecb70b99a
David Su <david.d.su@gmail.com>
2023-06-11 00:15:51 -0700

    Port modelPerform() from rave_vst

--- SoundEnginePlugin/RaveWwiseFX.cpp
+++ SoundEnginePlugin/RaveWwiseFX.cpp
@@ -29,8 +29,10 @@ the specific language governing permissions and limitations under the License.
 
 #include <AK/AkWwiseSDKVersion.h>
 
-#include <codecvt>
+#include <algorithm>
 #include <math.h>
+
+#define DEBUG_PERFORM 0
 
 AK::IAkPlugin* CreateRaveWwiseFX(AK::IAkPluginMemAlloc* in_pAllocator)
 {
@@ -173,8 +175,139 @@ AKRESULT RaveWwiseFX::TimeSkip(AkUInt32 &io_uFrames)
 
 void RaveWwiseFX::modelPerform()
 {
-	// TODO
+    if (_rave.get() && !_isMuted.load()) {
+        c10::InferenceMode guard(true);
+        // encode
+        int input_size = static_cast<int>(pow(2, *_latencyMode));
+
+        at::Tensor latent_traj;
+        at::Tensor latent_traj_mean;
+
+#if DEBUG_PERFORM
+        std::cout << "exp: " << *_latencyMode << " value: " << input_size << '\n';
+        std::cout << "has prior : " << _rave->hasPrior()
+            << "; use prior : " << *_usePrior << std::endl;
+        std::cout << "temperature : " << *_priorTemperature << std::endl;
+#endif
+
+        if (_rave->hasPrior() && *_usePrior) {
+            auto n_trajs = pow(2, *_latencyMode) / _rave->getModelRatio();
+            latent_traj = _rave->sample_prior((int)n_trajs, *_priorTemperature);
+            latent_traj_mean = latent_traj;
+        }
+        else {
+            int64_t sizes = { input_size };
+            at::Tensor frame = torch::from_blob(_inModel[0].get(), sizes);
+            frame = torch::reshape(frame, { 1, 1, input_size });
+#if DEBUG_PERFORM
+            std::cout << "Current input size : " << frame.sizes() << std::endl;
+#endif DEBUG_PERFORM
+            std::vector<torch::Tensor> latent_probs = _rave->encode_amortized(frame);
+            latent_traj_mean = latent_probs[0];
+            at::Tensor latent_traj_std = latent_probs[1];
+
+#if DEBUG_PERFORM
+            std::cout << "mean shape" << latent_traj_mean.sizes() << std::endl;
+            std::cout << "std shape" << latent_traj_std.sizes() << std::endl;
+#endif
+
+            latent_traj = latent_traj_mean +
+                latent_traj_std * torch::randn_like(latent_traj_mean);
+        }
+
+#if DEBUG_PERFORM
+        std::cout << "latent traj shape" << latent_traj.sizes() << std::endl;
+#endif
+
+        // Latent modifications
+        // apply scale and bias
+        int64_t n_dimensions =
+            std::min((int)latent_traj.size(1), (int)AVAILABLE_DIMS);
+        for (int i = 0; i < n_dimensions; i++) {
+            // The assert and casting here is needed as I got a:
+            // warning: conversion to ‘std::array<std::atomic<float>*,
+            // 8>::size_type’ {aka ‘long unsigned int’} from ‘int’ may change the
+            // sign of the result [-Wsign-conversion]
+            // Whatever AVAILABLE_DIMS type I defined
+            assert(i >= 0);
+            auto i2 = (long unsigned int)i;
+            float scale = _latentScale->at(i2)->load();
+            float bias = _latentBias->at(i2)->load();
+            latent_traj.index_put_({ 0, i },
+                (latent_traj.index({ 0, i }) * scale + bias));
+            latent_traj_mean.index_put_(
+                { 0, i }, (latent_traj_mean.index({ 0, i }) * scale + bias));
+        }
+        _rave->writeLatentBuffer(latent_traj_mean);
+
+#if DEBUG_PERFORM
+        std::cout << "scale & bias applied" << std::endl;
+#endif
+
+        // adding latent jitter on meaningful dimensions
+        float jitter_amount = _latentJitterValue->load();
+        latent_traj = latent_traj + jitter_amount * torch::randn_like(latent_traj);
+
+#if DEBUG_PERFORM
+        std::cout << "jitter applied" << std::endl;
+#endif
+
+        // filling missing dimensions with width parameter
+        torch::Tensor latent_trajL = latent_traj,
+            latent_trajR = latent_traj.clone();
+        int missing_dims = _rave->getFullLatentDimensions() - latent_trajL.size(1);
+        float width = _widthValue->load() / 100.f;
+        at::Tensor latent_noiseL =
+            torch::randn({ 1, missing_dims, latent_trajL.size(2) });
+        at::Tensor latent_noiseR =
+            (1 - width) * latent_noiseL +
+            width * torch::randn({ 1, missing_dims, latent_trajL.size(2) });
+
+#if DEBUG_PERFORM
+        std::cout << "after width : " << latent_noiseL.sizes() << ";"
+            << latent_trajL.sizes() << std::endl;
+#endif
+
+        latent_trajL = torch::cat({ latent_trajL, latent_noiseL }, 1);
+        latent_trajR = torch::cat({ latent_trajR, latent_noiseR }, 1);
+
+#if DEBUG_PERFORM
+        std::cout << "latent processed" << std::endl;
+#endif
+
+        // Decode
+        latent_traj = torch::cat({ latent_trajL, latent_trajR }, 0);
+        at::Tensor out = _rave->decode(latent_traj);
+        // On windows, I don't get why, but the two first dims are swapped (compared
+        // to macOS / UNIX) with the same torch version
+        if (out.sizes()[0] == 2) {
+            out = out.transpose(0, 1);
+        }
+        at::Tensor outL = out.index({ 0, 0, at::indexing::Slice() });
+        at::Tensor outR = out.index({ 0, 1, at::indexing::Slice() });
+
+#if DEBUG_PERFORM
+        std::cout << "latent decoded" << std::endl;
+#endif
+
+        float* outputDataPtrL, * outputDataPtrR;
+        outputDataPtrL = outL.data_ptr<float>();
+        outputDataPtrR = outR.data_ptr<float>();
+
+        // Write in buffers
+        assert(input_size >= 0);
+        for (size_t i = 0; i < (size_t)input_size; i++) {
+            _outModel[0][i] = outputDataPtrL[i];
+            _outModel[1][i] = outputDataPtrR[i];
+        }
+        if (_smoothedFadeInOut.getCurrentValue() < EPSILON) {
+            _isMuted.store(true);
+        }
+    }
 }
+
+void modelPerform_callback(RaveWwiseFX* ap) { ap->modelPerform(); }
+
 
 void RaveWwiseFX::detectAvailableModels()
 {
